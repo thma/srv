8ba39f1d392fe3eb71851073c5c4435c8838baef
Mahler, Thomas <thomas.mahler@ista.com>
2022-06-21 16:59:07 +0200

    poc works !

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import Lib
+import Network.Wai.Handler.Warp 
+import Network.Wai.Application.Static
+import Network.Wai.Handler.WarpTLS
 
 main :: IO ()
-main = someFunc
+main = do 
+   let tls = defaultTlsSettings  -- tlsSettings "pathToCert" "pathToKey"
+       app = (staticApp (defaultFileServerSettings "."))
+   --runTLS tls (setPort 443 defaultSettings) app
+   run 80 app
