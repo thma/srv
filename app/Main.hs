65dfb205a62a7dbaec3ef8a064805a4d2b4613b1
Mahler, Thomas <thomas.mahler@ista.com>
2022-12-07 14:32:16 +0100

    running https and http concurrently

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
-import Network.Wai.Handler.Warp 
-import Network.Wai.Application.Static
-import Network.Wai.Handler.WarpTLS
+import           Control.Concurrent.Async
+import           Network.Wai.Application.Static
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Handler.WarpTLS
 
 main :: IO ()
-main = do 
-   let tls = defaultTlsSettings  -- tlsSettings "pathToCert" "pathToKey"
-       app = (staticApp (defaultFileServerSettings "."))
-   --runTLS tls (setPort 443 defaultSettings) app
-   run 80 app
+main = do
+  let tls   = defaultTlsSettings -- tlsSettings "pathToCert" "pathToKey"
+      app   = staticApp (defaultFileServerSettings ".")
+      https = runTLS tls (setPort 443 defaultSettings) app
+      http  = run 80 app
+  _ <- concurrently http https
+  return ()
