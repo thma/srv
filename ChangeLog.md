ea1dc847eaba0a9a1e70a8bad00524df9819f33a
Mahler, Thomas <thomas.mahler@ista.com>
2022-12-08 11:41:51 +0100

    having a config.yaml

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for srv
-
-## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,49 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE LambdaCase     #-}
+
 module Main where
 
 import           Control.Concurrent.Async
+import qualified Data.Yaml                      as Yaml
+import           GHC.Generics                   (Generic)
 import           Network.Wai.Application.Static
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Handler.WarpTLS
 
-data WarpHandler = Http | Https | Both deriving (Show, Eq)
+data WarpHandler = Http | Https deriving (Show, Eq, Generic, Yaml.FromJSON, Yaml.ToJSON)
 
-data SrvConfig = SrvConfig {
-    handler :: WarpHandler
-  , httpPort :: Int
-  , httpsPort :: Int
-  , pathToCert :: FilePath
-  , pathToKey :: FilePath
-  , dir :: FilePath
-} deriving (Show, Eq)
+data SrvConfig = SrvConfig
+  { handlers   :: ![WarpHandler],
+    httpPort   :: !Int,
+    httpsPort  :: !Int,
+    pathToCert :: !FilePath,
+    pathToKey  :: !FilePath,
+    dir        :: !FilePath
+  }
+  deriving (Show, Eq, Generic, Yaml.FromJSON, Yaml.ToJSON)
 
 defaultConfig :: SrvConfig
-defaultConfig = SrvConfig {
-    handler = Both
-  , httpPort = 8080
-  , httpsPort = 443
-  , pathToCert = "certificate.pem"
-  , pathToKey  = "key.pem"
-  , dir = "."
-}
+defaultConfig =
+  SrvConfig
+    { handlers = [Http, Https],
+      httpPort = 8080,
+      httpsPort = 443,
+      pathToCert = "certificate.pem",
+      pathToKey = "key.pem",
+      dir = "."
+    }
 
 main :: IO ()
 main = do
   putStrLn "starting up srv..."
   let config = defaultConfig
-  print config
+  Yaml.encodeFile "config.yaml" config
 
-  let tls   = tlsSettings (pathToCert config) (pathToKey config)
-      app   = staticApp (defaultFileServerSettings (dir config))
+  let tls = tlsSettings (pathToCert config) (pathToKey config)
+      app = staticApp (defaultFileServerSettings (dir config))
       https = runTLS tls (setPort (httpsPort config) defaultSettings) app
-      http  = run (httpPort config) app
-  case handler config of
+      http = run (httpPort config) app
+  forConcurrently_ (handlers config) $ \case
     Http  -> http
     Https -> https
-    Both  -> concurrently_ http https
-
